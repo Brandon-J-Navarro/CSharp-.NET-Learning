04fe7d102923097c2e7b3315e195fab8587c1fef
Brandon <brandon.j.navarro@outlook.com>
2023-08-15 12:59:36 -0400

    Inital Post

--- .Net/WordUnscrambler/WordUnscrambler/Program.cs
+++ .Net/WordUnscrambler/WordUnscrambler/Program.cs
@@ -1,16 +1,97 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using WordUnscrambler.Workers;
+using WordUnscrambler.Data;
 
-namespace WordUnscambler
+namespace WordUnscrambler
 {
     class Program
     {
+        private static readonly FileReader _fileReader = new FileReader();
+        private static readonly WordMatcher _wordMatcher = new WordMatcher();
         static void Main(string[] args)
         {
+            try
+            {
+                bool continueWordUnscramble = true;
+                do
+                {
+                    Console.WriteLine(Constants.OptionOnHowToEnterScrambledWords);
+                    var option = Console.ReadLine() ?? string.Empty;
+                    switch (option.ToUpper())
+                    {
+                        case Constants.File:
+                            Console.Write(Constants.EnterScrambledWordsViaFile);
+                            ExecuteScrambledWordsInFileScenario();
+                            break;
+                        case Constants.Manual:
+                            Console.Write(Constants.EnterScrambledWordsManually);
+                            ExecuteScrambledWordsManualEntryScenario();
+                            break;
+                        default:
+                            Console.WriteLine(Constants.EnterScrambledWordsOptionNotReconized);
+                            break;
+                    }
+                    var continueDecision = string.Empty;
+                    do
+                    {
+                        Console.WriteLine(Constants.OptionOnContinuingTheProgam);
+                        continueDecision = (Console.ReadLine() ?? string.Empty);
+                    }
+                    while (!continueDecision.Equals(Constants.Yes, StringComparison.OrdinalIgnoreCase) &&
+                           !continueDecision.Equals(Constants.No, StringComparison.OrdinalIgnoreCase));
 
+                    continueWordUnscramble = continueDecision.Equals(Constants.Yes, StringComparison.OrdinalIgnoreCase);
+                }
+                while (continueWordUnscramble);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(Constants.ErrorProgramWillBeTerminated + ex.Message);
+            }
+
+        }
+
+        private static void ExecuteScrambledWordsManualEntryScenario()
+        {
+            try
+            {
+                var manualInput = Console.ReadLine() ?? string.Empty;
+                string[] scrambledWords = manualInput.Split(',');
+                DisplayMatchedUnscrambledWords(scrambledWords);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(Constants.ErrorScrambledWordsCannotBeLoaded + ex.Message);
+            }
+
+        }
+
+        private static void ExecuteScrambledWordsInFileScenario()
+        {
+            var fileName = Console.ReadLine() ?? string.Empty;
+            string[] scrambledWords = _fileReader.Read(fileName);
+            DisplayMatchedUnscrambledWords(scrambledWords);
+        }
+
+        private static void DisplayMatchedUnscrambledWords(string[] scrambledWords)
+        {
+            string[] wordList = _fileReader.Read(Constants.WordListFileName);
+
+            List<MatchedWord> matchedWords = _wordMatcher.Match(scrambledWords, wordList);
+
+            if(matchedWords.Any())
+            {
+                foreach(var matchedWord in matchedWords)
+                {
+                    Console.WriteLine(Constants.MatchFound, matchedWord.ScrambledWord, matchedWord.Word);
+                }
+            }
+            else
+            {
+                Console.WriteLine(Constants.MatchNotFound.ToUpper());
+            }
         }
     }
 }
