acd1d12a1391a69455b992d29d1e8aaeee54b840
Brandon <111472416+Brandon-J-Navarro@users.noreply.github.com>
2023-08-21 15:20:51 -0400

    Initial Commit

--- .Net/Master C# and SQL by Building Applications/Database Setup Query.sql
+++ .Net/Master C# and SQL by Building Applications/Database Setup Query.sql
@@ -1,6 +1,8 @@
 CREATE DATABASE CustomerOrderViewer;
+GO
 
 USE CustomerOrderViewer;
+GO
 
 CREATE TABLE [dbo].[Customer] (
     CustomerId INT IDENTITY(1,1) NOT NULL,
@@ -10,6 +12,7 @@ CREATE TABLE [dbo].[Customer] (
     Age INT NOT NULL,
     PRIMARY KEY(CustomerId)
 );
+GO
 
 CREATE TABLE [dbo].[Item] (
     ItemId INT IDENTITY(1,1) NOT NULL,
@@ -17,6 +20,7 @@ CREATE TABLE [dbo].[Item] (
     Price DECIMAL(4,2) NOT NULL,
     PRIMARY KEY (ItemId)
 );
+GO
 
 CREATE TABLE [dbo].[CustomerOrder] (
     CustomerOrderId INT IDENTITY(1,1) NOT NULL,
@@ -26,6 +30,7 @@ CREATE TABLE [dbo].[CustomerOrder] (
     FOREIGN KEY (CustomerId) REFERENCES Customer(CustomerId),
     FOREIGN KEY (ItemId) REFERENCES Item(ItemId)
 );
+GO
 
 -- [] Used when using reserved key words such as 'dbo' or 'Description'
 -- Feature 'Identity(1,1)' starts at 1 and adds one to each new customer
@@ -43,6 +48,7 @@ INSERT INTO [dbo].[Customer] (FirstName, MiddleName, LastName, Age) VALUES ('Alf
 INSERT INTO [dbo].[Customer] (FirstName, MiddleName, LastName, Age) VALUES ('Megan', NULL, 'Read', 22);
 INSERT INTO [dbo].[Customer] (FirstName, MiddleName, LastName, Age) VALUES ('Melissa', NULL, 'Sherlock', 31);
 INSERT INTO [dbo].[Customer] (FirstName, MiddleName, LastName, Age) VALUES ('Kori', NULL, 'Higgins', 57);
+GO
 
 INSERT INTO [dbo].[Item] ([Description], Price) VALUES ('Duracell Alkaline AA Batteries', 15.99);
 INSERT INTO [dbo].[Item] ([Description], Price) VALUES ('Kirkland Signature Moist Flushable Wipes', 14.99);
@@ -54,6 +60,7 @@ INSERT INTO [dbo].[Item] ([Description], Price) VALUES ('Mountain Dew', 9.99);
 INSERT INTO [dbo].[Item] ([Description], Price) VALUES ('Pepsi Cola', 9.99);
 INSERT INTO [dbo].[Item] ([Description], Price) VALUES ('Sprite', 10.99);
 INSERT INTO [dbo].[Item] ([Description], Price) VALUES ('San Pellegrino Mineral Water', 17.99);
+GO
 
 INSERT INTO [dbo].[CustomerOrder] (CustomerId, ItemId) VALUES (1, 1);
 INSERT INTO [dbo].[CustomerOrder] (CustomerId, ItemId) VALUES (1, 2);
@@ -80,7 +87,7 @@ INSERT INTO [dbo].[CustomerOrder] (CustomerId, ItemId) VALUES (9, 3);
 INSERT INTO [dbo].[CustomerOrder] (CustomerId, ItemId) VALUES (9, 4);
 INSERT INTO [dbo].[CustomerOrder] (CustomerId, ItemId) VALUES (9, 5);
 INSERT INTO [dbo].[CustomerOrder] (CustomerId, ItemId) VALUES (9, 6);
-
+GO
 
 CREATE VIEW [dbo].[CustomerOrderDetail] AS
     SELECT
@@ -97,3 +104,4 @@ CREATE VIEW [dbo].[CustomerOrderDetail] AS
         dbo.Customer t2 ON t2.CustomerId = t1.CustomerId
     INNER JOIN
         dbo.Item t3 ON t3.ItemId = t1.ItemId;
+GO
