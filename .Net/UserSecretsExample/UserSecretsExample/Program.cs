7f6538ae298d2d17815a6029a58d2257989f7b05
Brandon <111472416+Brandon-J-Navarro@users.noreply.github.com>
2023-08-23 10:54:27 -0400

    Initial Commit

---
+++ .Net/UserSecretsExample/UserSecretsExample/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Extensions.Configuration;
+
+
+namespace UserSecretsExample
+{
+    class Program
+    {
+        private static readonly IConfiguration _configuration = Startup.BuildConfiguation();
+        private static string _connectionString = _configuration["ConnectionString:sqlConnectionString"];
+    }
+}
