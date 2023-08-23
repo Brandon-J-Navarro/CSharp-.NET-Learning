bc01c702826350faef0181458f813f8bf1ebf604
Brandon <111472416+Brandon-J-Navarro@users.noreply.github.com>
2023-08-23 14:30:39 -0400

    initial commit

---
+++ .Net/Master C# and SQL by Building Applications/CourseReportEmailer/CourseReportEmailer/Startup.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CourseReportEmailer
+{
+    internal class Startup
+    {
+        public static IConfiguration BuildConfiguation()
+        {
+            return new ConfigurationBuilder()
+                .AddUserSecrets<Program>()
+                .Build();
+        }
+        public static IServiceProvider ConfigureService()
+        {
+            var provider = new ServiceCollection()
+                .BuildServiceProvider();
+            return provider;
+        }
+    }
+}
