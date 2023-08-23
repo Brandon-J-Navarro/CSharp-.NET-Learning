2fcedb6823b50da060ba58796b3caac1d13bd85d
Brandon <111472416+Brandon-J-Navarro@users.noreply.github.com>
2023-08-23 10:15:44 -0400

    Changed SQL Connection String to use User Secrets and other typo mistakes

---
+++ .Net/Master C# and SQL by Building Applications/CustomerOrderViewer2.0/CustomerOrderViewer2.0/Startup.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerOrderViewer2._0
+{
+    internal class Startup
+    {
+        public static IConfiguration BuildConfiguation()
+        {
+            return new ConfigurationBuilder()
+                .AddUserSecrets<Program>()
+                .Build();
+        }
+        public static IServiceProvider ConfigureService()
+        {
+            var provider = new ServiceCollection()
+                .BuildServiceProvider();
+            return provider;
+        }
+    }
+}
