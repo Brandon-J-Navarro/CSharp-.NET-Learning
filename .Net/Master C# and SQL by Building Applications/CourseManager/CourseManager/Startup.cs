db3c2a735918c53f1c7cc83d8c69c6f18f21a3a4
Brandon <111472416+Brandon-J-Navarro@users.noreply.github.com>
2023-08-24 09:08:34 -0400

    course complete

--- .Net/Master C# and SQL by Building Applications/CourseManager/CourseManager/Startup.cs
+++ .Net/Master C# and SQL by Building Applications/CourseManager/CourseManager/Startup.cs
@@ -1,5 +1,7 @@
 ﻿using Caliburn.Micro;
 using CourseManager.ViewModels;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -18,6 +20,18 @@ namespace CourseManager
         protected override void OnStartup(object sender, StartupEventArgs e)
         {
             DisplayRootViewForAsync<MainViewModel>();
+        }
+        public static IConfiguration BuildConfiguation()
+        {
+            return new ConfigurationBuilder()
+                .AddUserSecrets<MainViewModel>()
+                .Build();
+        }
+        public static IServiceProvider ConfigureService()
+        {
+            var provider = new ServiceCollection()
+                .BuildServiceProvider();
+            return provider;
         }
     }
 }
