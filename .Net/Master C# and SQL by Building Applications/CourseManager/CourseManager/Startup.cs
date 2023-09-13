fb44d3b5b6543e225a05509be018d357e4c56184
Brandon <111472416+Brandon-J-Navarro@users.noreply.github.com>
2023-09-13 13:01:27 -0400

    Revert "Revert "merge staged to main""

---
+++ .Net/Master C# and SQL by Building Applications/CourseManager/CourseManager/Startup.cs
@@ -0,0 +1,37 @@
+﻿using Caliburn.Micro;
+using CourseManager.ViewModels;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace CourseManager
+{
+    internal class Startup : BootstrapperBase
+    {
+        public Startup()
+        {
+            Initialize();
+        }
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewForAsync<MainViewModel>();
+        }
+        public static IConfiguration BuildConfiguation()
+        {
+            return new ConfigurationBuilder()
+                .AddUserSecrets<MainViewModel>()
+                .Build();
+        }
+        public static IServiceProvider ConfigureService()
+        {
+            var provider = new ServiceCollection()
+                .BuildServiceProvider();
+            return provider;
+        }
+    }
+}
