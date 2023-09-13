087d7b999a18f79327a98a7fe38551e63bf2c5e6
Brandon <111472416+Brandon-J-Navarro@users.noreply.github.com>
2023-09-13 13:00:28 -0400

    Revert "update example"
    This reverts commit 52d4026c2c25fb261265de76e2e8caf343378ba7.

--- .Net/UserSecretsExample/UserSecretsExample/Program.cs
+++ .Net/UserSecretsExample/UserSecretsExample/Program.cs
@@ -3,12 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using Microsoft.Extensions.Configuration;
 
-// NuGet Packages Required:
-//      Microsoft.Extensions.Configuration.CommandLine,
-//      Microsoft.Extensions.Configuration.EnvironmentVariables,
-//      Microsoft.Extensions.Configuration.Json,
-//      Microsoft.Extensions.Configuration.UserSecrets,
-//      Microsoft.Extensions.DependencyInjection
 
 namespace UserSecretsExample
 {
@@ -16,7 +10,5 @@ namespace UserSecretsExample
     {
         private static readonly IConfiguration _configuration = Startup.BuildConfiguation();
         private static string _connectionString = _configuration["ConnectionString:sqlConnectionString"];
-        private static string _password = _configuration["EmailCredentials:password"];
-        private static string _from = _configuration["EmailCredentials:email"];
     }
 }
